d894bccf9392a3ccecac9a151938ed1e5d4ae976
MateusLino <mateuspireslino@gmail.com>
2024-11-05 21:20:20 -0300

    Atualização de codigo

--- js/main.js
+++ js/main.js
@@ -3,24 +3,20 @@ $(document).ready(function () {
         e.preventDefault(); // impedir o recaregamento automatico
 
         // pegar valor que o usuário vai digitar
-<<<<<<< HEAD
         const inputTarefa = $('#digitar_tarefa').val().trim(); // trim remove espaços em branco 
-=======
-        const inputTarefa = $('#digitar_tarefa').val().trim();
->>>>>>> 725d3c8e7f5a676c48f7684d4e0f563a876318fc
 
         //Verificar se o campo de entrada não esta vazio
         if (inputTarefa) {
 
             //criar um novo item de lista e o adiciona ao <ul>
-            $('<li>' + inputTarefa + '</li>').appendTo('ul');
+            $('<li class="list_riscar">' + inputTarefa + '</li>').appendTo('ul');
 
             // limpar campo de entrada após adicionar tarefa
             $('#digitar_tarefa').val('');
         }
     });
     //Riscar a tarefa ao clicar nela 
-    $('ul').on('click', 'li', function () {
+    $('ul').on('click', '.list_riscar', function () {
         $(this).toggleClass('line-through');
     });
 })
